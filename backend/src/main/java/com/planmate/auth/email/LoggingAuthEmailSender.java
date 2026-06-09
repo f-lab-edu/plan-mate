093d351b14b5b0146f9ff889515c193d00ba6f26
LJS-99 <lkhejj1@naver.com>
2026-06-09 17:10:41 +0900

    feat: Gmail SMTP 이메일 인증 발송 구현

--- backend/src/main/java/com/planmate/auth/email/LoggingAuthEmailSender.java
+++ backend/src/main/java/com/planmate/auth/email/LoggingAuthEmailSender.java
@@ -3,10 +3,12 @@ package com.planmate.auth.email;
 import com.planmate.user.entity.UserEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
+@ConditionalOnProperty(name = "app.mail.provider", havingValue = "logging", matchIfMissing = true)
 public class LoggingAuthEmailSender implements AuthEmailSender {
 
     private static final Logger log = LoggerFactory.getLogger(LoggingAuthEmailSender.class);
